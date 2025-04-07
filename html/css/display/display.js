3bd9069d5e364380d8060ace6cd5f7c3c875e618
Shinichiro Yabu <scin.ichi@gmail.com>
2025-04-07 11:08:59 +0900

    ✨ computedStyleとの比較を追加

--- html/css/display/display.js
+++ html/css/display/display.js
@@ -1,6 +1,7 @@
 const templateSection = document.querySelector(".section");
 templateSection.remove();
 
+/** CSSで使用しているクラス名。`--` を ` ` に置換すると display プロパティの値になる。 */
 const classNames = [
 	"block",
 	"inline",
@@ -24,9 +25,14 @@ const classNames = [
 	"table-row",
 	"list-item",
 ];
+
+/** 結果を格納する写像。キーは計算値、値は display プロパティの値の集合。 */
+const styleMap = new Map();
+
 for (const className of classNames) {
+	const display = className.replace(/--/, " ");
 	const section = templateSection.cloneNode(true);
-	section.querySelector("h2").textContent = className.replace(/--/, " ");
+	section.querySelector("h2").textContent = display;
 	for (const item of section.querySelectorAll(".item")) {
 		item.classList.add(className);
 	}
@@ -34,10 +40,25 @@ for (const className of classNames) {
 
 	const h2 = section.querySelector("h2");
 	const item = section.querySelector(".item");
-	const style = window.getComputedStyle(item);
-	const display = style.display;
+	const computedStyle = window.getComputedStyle(item);
 	const result = document.createElement("span");
 	result.classList.add("result");
-	result.textContent = display;
+	result.textContent = computedStyle.display;
 	h2.appendChild(result);
+
+	if (!styleMap.has(computedStyle.display)) {
+		styleMap.set(computedStyle.display, new Set());
+	}
+	styleMap.get(computedStyle.display).add(display);
+	console.log(computedStyle.display, "←", display);
 }
+
+const resultDiv = document.getElementById("result");
+const resultList = document.createElement("ol");
+for (const [key, value] of styleMap.entries()) {
+	const line = `${key} ← ${Array.from(value).join(" / ")}`;
+	const li = document.createElement("li");
+	li.textContent = line;
+	resultList.appendChild(li);
+}
+resultDiv.appendChild(resultList);
